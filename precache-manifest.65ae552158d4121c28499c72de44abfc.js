f78f5139b699067fb7d66b0e94d5244daf3d70d0
Jomark Pangan <jomarkrazonpangan@gmail.com>
2020-07-14 18:33:28 +0800

    Updates

---
+++ precache-manifest.65ae552158d4121c28499c72de44abfc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060f224d6d0241d8af0572ed5a6f6a22",
+    "url": "/jomark-portfolio/index.html"
+  },
+  {
+    "revision": "5565ba6997c12e7c38a2",
+    "url": "/jomark-portfolio/static/css/main.3a4d39f3.chunk.css"
+  },
+  {
+    "revision": "2c293d327394c83f568c",
+    "url": "/jomark-portfolio/static/js/2.7de4d663.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/jomark-portfolio/static/js/2.7de4d663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5565ba6997c12e7c38a2",
+    "url": "/jomark-portfolio/static/js/main.38b2b8e8.chunk.js"
+  },
+  {
+    "revision": "38d71420f9a8f5eca10d",
+    "url": "/jomark-portfolio/static/js/runtime-main.2b713253.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/jomark-portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "6fa3728be823292ebd6e1482ef2f8470",
+    "url": "/jomark-portfolio/static/media/jomark.6fa3728b.jpg"
+  }
+]);
